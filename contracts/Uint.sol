db4927c632ad37f0ec6e9affde9bdd2251ed1054
Chamodpw2000 <mailtochamodw@gmail.com>
2025-06-15 14:01:51 +0530

    int and uint section completed

---
+++ contracts/Uint.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.14;
+
+contract ExampleUint {
+    uint256 public myUint; // 0 - (2^256)-1
+    uint8 public myUint8 = 254; //255 is maximum
+    int public myInt = -10;
+
+    function setMyUint(uint _myUint) public {
+        myUint = _myUint;
+    }
+
+    function decrementUint() public {
+        myUint--;
+    }
+
+    function incrementUint8() public{
+        myUint8++; // if try to increment more than 255 throws an error
+    }
+
+    function incrementInt() public {
+        myInt++;
+    }
+}
